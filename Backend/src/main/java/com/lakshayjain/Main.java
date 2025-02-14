27507893b0f2491f325fb99a98cfb447c78833ac
lakshayyjain <lakshayjain312@gmail.com>
2025-02-14 23:07:22 +0530

    added new line and removed unused import

--- Backend/src/main/java/com/lakshayjain/Main.java
+++ Backend/src/main/java/com/lakshayjain/Main.java
@@ -10,8 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 @SpringBootApplication
