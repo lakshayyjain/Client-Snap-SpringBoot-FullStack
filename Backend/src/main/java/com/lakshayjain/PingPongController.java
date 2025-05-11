5c0812a219d2364ef965a1abd5685b68ac27ac92
Lakshay Jain <130989106+lakshayyjain@users.noreply.github.com>
2025-05-12 01:57:22 +0530

    update pingpong to test cd

--- Backend/src/main/java/com/lakshayjain/PingPongController.java
+++ Backend/src/main/java/com/lakshayjain/PingPongController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PingPongController {
+
     record PingPong(String result){}
-    
+
     @GetMapping("/ping")
     public PingPong getPingPing() {
         return new PingPong("pong");
     }
-    
-    
+
 }
