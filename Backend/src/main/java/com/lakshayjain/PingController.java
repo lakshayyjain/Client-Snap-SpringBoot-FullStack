5703936015187b1de39f6cbfaef65c8d9c81e561
Lakshay Jain <130989106+lakshayyjain@users.noreply.github.com>
2025-05-17 18:06:26 +0530

    final testing

--- Backend/src/main/java/com/lakshayjain/PingController.java
+++ Backend/src/main/java/com/lakshayjain/PingController.java
@@ -5,10 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PingController {
-
     private static int counter = 0;
     record PingPong(String result){}
-
     @GetMapping("/ping")
     public PingPong getPingPing() {
         return new PingPong("pong: %s".formatted(++counter));
